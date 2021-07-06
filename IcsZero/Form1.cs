376b8958781fb4fc3ab1150a3e5d6ee0cc061881
antoniu.pitic <antoniu.pitic@gmail.com>
2021-07-06 11:41:26 +0300

    - click pe 1 label

---
+++ IcsZero/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IcsZero
+{
+    public partial class Form1 : Form
+    {
+        bool b = true;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            if (b)
+            {
+                label1.Text = "0";
+                b = !b;
+            }
+            else
+            {
+                label1.Text = "1";
+                b = !b;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
