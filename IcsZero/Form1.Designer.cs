f13e99faf632e0015fd630c0baa40ab090133025
antoniu.pitic <antoniu.pitic@gmail.com>
2021-07-06 11:52:29 +0300

    - primele 2 celule testate

--- IcsZero/Form1.Designer.cs
+++ IcsZero/Form1.Designer.cs
@@ -44,7 +44,7 @@ namespace IcsZero
             // 
             this.label1.BackColor = System.Drawing.Color.Lime;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(166, 119);
+            this.label1.Location = new System.Drawing.Point(167, 119);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(50, 48);
             this.label1.TabIndex = 0;
@@ -60,6 +60,7 @@ namespace IcsZero
             this.label2.Size = new System.Drawing.Size(50, 48);
             this.label2.TabIndex = 1;
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label3
             // 
@@ -70,16 +71,18 @@ namespace IcsZero
             this.label3.Size = new System.Drawing.Size(50, 48);
             this.label3.TabIndex = 2;
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // label4
             // 
             this.label4.BackColor = System.Drawing.Color.Lime;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(278, 176);
+            this.label4.Location = new System.Drawing.Point(167, 176);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(50, 48);
             this.label4.TabIndex = 5;
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // label5
             // 
@@ -90,26 +93,29 @@ namespace IcsZero
             this.label5.Size = new System.Drawing.Size(50, 48);
             this.label5.TabIndex = 4;
             this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
             // label6
             // 
             this.label6.BackColor = System.Drawing.Color.Lime;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(166, 176);
+            this.label6.Location = new System.Drawing.Point(278, 176);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(50, 48);
             this.label6.TabIndex = 3;
             this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label6.Click += new System.EventHandler(this.label6_Click);
             // 
             // label7
             // 
             this.label7.BackColor = System.Drawing.Color.Lime;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(278, 233);
+            this.label7.Location = new System.Drawing.Point(166, 233);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(50, 48);
             this.label7.TabIndex = 8;
             this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label7.Click += new System.EventHandler(this.label7_Click);
             // 
             // label8
             // 
@@ -120,16 +126,18 @@ namespace IcsZero
             this.label8.Size = new System.Drawing.Size(50, 48);
             this.label8.TabIndex = 7;
             this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label8.Click += new System.EventHandler(this.label8_Click);
             // 
             // label9
             // 
             this.label9.BackColor = System.Drawing.Color.Lime;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(166, 233);
+            this.label9.Location = new System.Drawing.Point(278, 233);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(50, 48);
             this.label9.TabIndex = 6;
             this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label9.Click += new System.EventHandler(this.label9_Click);
             // 
             // Form1
             // 
