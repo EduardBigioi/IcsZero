9834117e8c4ac20cb3f49cf223df61c3c5884000
antoniu.pitic <antoniu.pitic@gmail.com>
2021-07-07 10:35:20 +0300

    - refactoring pe initializare tabla de joc

--- IcsZero/Form1.cs
+++ IcsZero/Form1.cs
@@ -36,49 +36,40 @@ namespace IcsZero
                     label1.Text = "1";
                     b = !b;
                 }
+
+
             if (label1.Text == label2.Text && label2.Text == label3.Text)
             {
                 if (label1.Text == "0") MessageBox.Show("A castigat Toni");
                 else MessageBox.Show("A castigat Dorel");
-                label1.Text = "";
-                label2.Text = "";
-                label3.Text = "";
-                label4.Text = "";
-                label5.Text = "";
-                label6.Text = "";
-                label7.Text = "";
-                label8.Text = "";
-                label9.Text = "";
+                ResetBoard();
             }
             if (label1.Text == label4.Text && label4.Text == label7.Text)
             {
                 if (label1.Text == "0") MessageBox.Show("A castigat Toni");
                 else MessageBox.Show("A castigat Dorel");
-                label1.Text = "";
-                label2.Text = "";
-                label3.Text = "";
-                label4.Text = "";
-                label5.Text = "";
-                label6.Text = "";
-                label7.Text = "";
-                label8.Text = "";
-                label9.Text = "";
+                ResetBoard();
             }
             if (label1.Text == label5.Text && label5.Text == label9.Text)
             {
                 if (label1.Text == "0") MessageBox.Show("A castigat Toni");
                 else MessageBox.Show("A castigat Dorel");
-                label1.Text = "";
-                label2.Text = "";
-                label3.Text = "";
-                label4.Text = "";
-                label5.Text = "";
-                label6.Text = "";
-                label7.Text = "";
-                label8.Text = "";
-                label9.Text = "";
+                ResetBoard();
             }
 
+        }
+
+        private void ResetBoard()
+        {
+            label1.Text = "";
+            label2.Text = "";
+            label3.Text = "";
+            label4.Text = "";
+            label5.Text = "";
+            label6.Text = "";
+            label7.Text = "";
+            label8.Text = "";
+            label9.Text = "";
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -90,151 +81,135 @@ namespace IcsZero
         {
             if (label2.Text == "")
                 if (b)
-            {
-                label2.Text = "0";
-                b = !b;
-            }
-            else
-            {
-                label2.Text = "1";
-                b = !b;
-            }
+                {
+                    label2.Text = "0";
+                    b = !b;
+                }
+                else
+                {
+                    label2.Text = "1";
+                    b = !b;
+                }
 
             if (label1.Text == label2.Text && label2.Text == label3.Text)
             {
                 if (label1.Text == "0") MessageBox.Show("A castigat Toni");
                 else MessageBox.Show("A castigat Dorel");
-                label1.Text = "";
-                label2.Text = "";
-                label3.Text = "";
-                label4.Text = "";
-                label5.Text = "";
-                label6.Text = "";
-                label7.Text = "";
-                label8.Text = "";
-                label9.Text = "";
+                ResetBoard();
             }
             if (label2.Text == label5.Text && label5.Text == label8.Text)
             {
                 if (label1.Text == "0") MessageBox.Show("A castigat Toni");
                 else MessageBox.Show("A castigat Dorel");
-                label1.Text = "";
-                label2.Text = "";
-                label3.Text = "";
-                label4.Text = "";
-                label5.Text = "";
-                label6.Text = "";
-                label7.Text = "";
-                label8.Text = "";
-                label9.Text = "";
+                ResetBoard();
             }
-          
+
         }
 
         // TODO faceti restul sa mearga :(
         private void label3_Click(object sender, EventArgs e)
         {
-            if (label3.Text == "")
-                if (b)
-            {
-                label3.Text = "0";
-                b = !b;
-            }
-            else
-            {
-                label3.Text = "1";
-                b = !b;
-            }
+                if (label3.Text == "")
+                    if (b)
+                    {
+                        label3.Text = "0";
+                        b = !b;
+                    }
+                    else
+                    {
+                        label3.Text = "1";
+                        b = !b;
+                    }
         }
 
         private void label6_Click(object sender, EventArgs e)
         {
             if (label6.Text == "")
                 if (b)
-            {
-                label6.Text = "0";
-                b = !b;
-            }
-            else
-            {
-                label6.Text = "1";
-                b = !b;
-            }
+                {
+                    label6.Text = "0";
+                    b = !b;
+                }
+                else
+                {
+                    label6.Text = "1";
+                    b = !b;
+                }
         }
 
         private void label5_Click(object sender, EventArgs e)
         {
             if (label5.Text == "")
                 if (b)
-            {
-                label5.Text = "0";
-                b = !b;
-            }
-            else
-            {
-                label5.Text = "1";
-                b = !b;
-            }
+                {
+                    label5.Text = "0";
+                    b = !b;
+                }
+                else
+                {
+                    label5.Text = "1";
+                    b = !b;
+                }
         }
 
         private void label4_Click(object sender, EventArgs e)
         {
             if (label4.Text == "")
                 if (b)
-            {
-                label4.Text = "0";
-                b = !b;
-            }
-            else
-            {
-                label4.Text = "1";
-                b = !b;
-            }
+                {
+                    label4.Text = "0";
+                    b = !b;
+                }
+                else
+                {
+                    label4.Text = "1";
+                    b = !b;
+                }
         }
 
         private void label9_Click(object sender, EventArgs e)
         {
             if (label9.Text == "")
                 if (b)
-            {
-                label9.Text = "0";
-                b = !b;
-            }
-            else
-            {
-                label9.Text = "1";
-                b = !b;
-            }
+                {
+                    label9.Text = "0";
+                    b = !b;
+                }
+                else
+                {
+                    label9.Text = "1";
+                    b = !b;
+                }
         }
 
         private void label8_Click(object sender, EventArgs e)
         {
             if (label8.Text == "")
                 if (b)
-            {
-                label8.Text = "0";
-                b = !b;
-            }
-            else
-            {
-                label8.Text = "1";
-                b = !b;
-            }
+                {
+                    label8.Text = "0";
+                    b = !b;
+                }
+                else
+                {
+                    label8.Text = "1";
+                    b = !b;
+                }
         }
 
         private void label7_Click(object sender, EventArgs e)
         {
             if (label7.Text == "")
                 if (b)
-            {
-                label7.Text = "0";
-                b = !b;
-            }
-            else
-            {
-                label7.Text = "1";
-                b = !b;
-            }
+                {
+                    label7.Text = "0";
+                    b = !b;
+                }
+                else
+                {
+                    label7.Text = "1";
+                    b = !b;
+                }
         }
     }
 }
